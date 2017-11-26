c963f4abef8851bebdc77505a5d00d54afb88416
Bahjazz <bahja.ahmed111@outlook.com>
2017-11-26 14:57:21 +0100

    har formulerat en sql fråga för att hämta all information

--- uppgifter/login/submit.php
+++ uppgifter/login/submit.php
@@ -7,7 +7,7 @@ $password = $_POST['password'];
 
 $dbc = mysqli_connect("localhost","root","","register");
 
-
+$query = "SELECT * FROM register WHERE username='$username' AND password='$password';";
 
 
 ?>
