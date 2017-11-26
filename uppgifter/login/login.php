194bdaa9311937ea167e0c2d2d564f5bc1b871dc
Bahjazz <bahja.ahmed111@outlook.com>
2017-11-26 14:24:56 +0100

    jag har formatterat koden

---
+++ uppgifter/login/login.php
@@ -0,0 +1,48 @@
+<!doctype html>
+
+<html>
+	<head>
+
+	<meta charset="UTF-8">
+	<title>login-formulär</title>
+ </head>
+<body>
+    <form action="submit.php" method="post"> 
+        <h3>Användarnamn:<h3/>
+		<input type="text" name="Användarnamn">
+        <h3>Lösenord:<h3/>
+		<input type="password" name="Lösenord">
+            <br>
+        <input type="submit" name="submit">
+        
+        
+        
+    </form>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
