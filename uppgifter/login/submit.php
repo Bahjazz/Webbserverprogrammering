db8473edccf261a3814d925c5ffd19e6b54de60d
Bahjazz <bahja.ahmed111@outlook.com>
2017-11-26 15:04:50 +0100

    villkor till ifall om det funkar att hämta reultat

--- uppgifter/login/submit.php
+++ uppgifter/login/submit.php
@@ -9,6 +9,13 @@ $dbc = mysqli_connect("localhost","root","","register");
 
 $query = "SELECT * FROM register WHERE username='$username' AND password='$password';";
 
+  if(mysqli_query($dbc,$query)){
+    echo "Successfully logged in!";
+ } 
+  else{
+    echo "failed try again!";
+    echo "<br>".$query;
+
 
 ?>
 
