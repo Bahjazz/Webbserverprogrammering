21db788e1564ec2e72fe5c2e3869004ffce4ebf0
Bahjazz <bahja.ahmed111@outlook.com>
2018-01-16 16:39:34 +0100

    fråga 1

---
+++ javascript/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+    
+    <title>javascript</title>
+    
+    </head>
+    
+    <script>
+    function Visameddelande(){
+        
+        alert("Mitt meddelande")
+    }
+    
+    
+    </script>
+    <body>
+    
+        <button onclick="VisaMeddelande()" >Bahja</button>
+        
+        
+        
+        
+    </body>
+    
+</html>
+
+ 
