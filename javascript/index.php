9d9ab109adcadaefdd0281df1b29a2d075cc5046
Bahjazz <bahja.ahmed111@outlook.com>
2018-01-19 10:19:19 +0100

    fråga 2

--- javascript/index.php
+++ javascript/index.php
@@ -5,21 +5,32 @@
     <title>javascript</title>
     
     </head>
+	
     
     <script>
     function Visameddelande(){
         
         alert("Mitt meddelande")
     }
-    
+   function storBild(x) {
+    x.style.height = "64px";
+    x.style.width = "64px";
+}
+
+   function normalBild(x) {
+    x.style.height = "32px";
+    x.style.width = "32px";
+}
     
     </script>
     <body>
-    
+        
         <button onclick="VisaMeddelande()" >Bahja</button>
-        
-        
-        
+		      <hr></hr>
+		<div>
+        <img onmouseover="storBild(this)" onmouseout="normalBild(this)" border="0" src="po.png" alt="po.png" width="32" height="32">
+        </div>
+                   <hr></hr>
         
     </body>
     
