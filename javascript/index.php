96f191580c16bc634f2d480caa152a609fd92d73
Bahjazz <bahja.ahmed111@outlook.com>
2018-01-19 11:09:16 +0100

    fråga 3

--- javascript/index.php
+++ javascript/index.php
@@ -21,16 +21,32 @@
     x.style.height = "32px";
     x.style.width = "32px";
 }
-    
+  
+	 function test(){
+			 var x = document.getElementById("Bild");
+			 if(x.style.display === "none"){
+				x.style.display = "block";
+			 }
+			 else{
+				 x.style.display = "none";
+			 }
+			 
+		 }
+	
     </script>
     <body>
         
         <button onclick="VisaMeddelande()" >Bahja</button>
 		      <hr></hr>
+			
 		<div>
         <img onmouseover="storBild(this)" onmouseout="normalBild(this)" border="0" src="po.png" alt="po.png" width="32" height="32">
         </div>
                    <hr></hr>
+	   <button onclick="test()">click</button>
+	    <img id="Bild" src="po.png"alt "po.png" width="32" height="32">
+		         <hr></hr>
+
         
     </body>
     
